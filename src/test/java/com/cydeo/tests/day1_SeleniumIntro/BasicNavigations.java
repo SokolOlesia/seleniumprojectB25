d8824dd240d389bb56fc5f54fee33b3569f24405
Olesia Sokol <lesy0101@me.com>
2022-02-04 22:24:06 +0700

    day1_SeleniumIntro

--- src/test/java/com/cydeo/tests/day1_SeleniumIntro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day1_SeleniumIntro/BasicNavigations.java
@@ -5,12 +5,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigations {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://tesla.com");
+
+        //Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        //navigate back
+        driver.navigate().back();
+
+
 
 
     }
