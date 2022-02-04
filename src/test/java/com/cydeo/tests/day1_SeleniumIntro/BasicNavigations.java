4cabdb7778e96669150409c3569305df15270930
Olesia Sokol <lesy0101@me.com>
2022-02-04 22:34:59 +0700

    day1_SeleniumIntro

--- src/test/java/com/cydeo/tests/day1_SeleniumIntro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day1_SeleniumIntro/BasicNavigations.java
@@ -12,14 +12,33 @@ public class BasicNavigations {
 
         driver.get("https://tesla.com");
 
+        String currentTitle = driver.getTitle();
+
         //Stop code execution for 3 seconds
         Thread.sleep(3000);
 
         //navigate back
         driver.navigate().back();
 
+        //Stop code execution for 3 seconds
+        Thread.sleep(3000);
 
+        //navigate forward
+        driver.navigate().forward();
 
+        //Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        //refresh
+        driver.navigate().refresh();
+
+        //use navigate().to();
+        driver.navigate().to("https://www.google.com");
+
+        //getTitle()
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+
+        System.out.println("currentTitle = " + currentTitle);
 
     }
 }
