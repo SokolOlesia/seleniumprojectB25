dc9fc9dacf08042e333607cec6c85ab2ca3da086
Olesia Sokol <lesy0101@me.com>
2022-02-04 22:18:53 +0700

    day1_SeleniumIntro

--- src/test/java/com/cydeo/tests/day1_SeleniumIntro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day1_SeleniumIntro/BasicNavigations.java
@@ -1,4 +1,17 @@
 package com.cydeo.tests.day1_SeleniumIntro;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BasicNavigations {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://tesla.com");
+
+
+    }
 }
