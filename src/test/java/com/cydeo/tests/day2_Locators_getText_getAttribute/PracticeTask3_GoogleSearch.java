c789f88046b33605a30baa045d64ff0759b9c780
Olesia Sokol <lesy0101@me.com>
2022-02-07 11:30:04 +0700

    day2_Locators_getText_getAttribute

--- src/test/java/com/cydeo/tests/day2_Locators_getText_getAttribute/PracticeTask3_GoogleSearch.java
+++ src/test/java/com/cydeo/tests/day2_Locators_getText_getAttribute/PracticeTask3_GoogleSearch.java
@@ -1,7 +1,42 @@
 package com.cydeo.tests.day2_Locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class PracticeTask3_GoogleSearch {
     public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://google.com");
 
+        //3- Write “apple” in search box
+        //4- Press Enter using Keys.Enter
+        WebElement googleSearchBox = driver.findElement(By.name("q"));
+        googleSearchBox.sendKeys("apple" + Keys.ENTER);
+
+        String expectedTitle = "apple";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.startsWith(expectedTitle)) {
+            System.out.println("Title verification PASSED");
+        } else {
+            System.out.println("Title verification FAILED!!!");
+        }
+
+        driver.close();
     }
 }
+/*
+TC#3: Google search
+1- Open a chrome browser
+2- Go to: https://google.com
+3- Write “apple” in search box
+4- Click google search button
+5- Verify title:
+Expected: Title should start with “apple” word
+ */
