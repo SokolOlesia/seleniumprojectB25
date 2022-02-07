636cae528d642dde3502dcd0c962267538c6f320
Olesia Sokol <lesy0101@me.com>
2022-02-07 10:34:30 +0700

    day2_Locators_getText_getAttribute

---
+++ src/test/java/com/cydeo/tests/day2_Locators_getText_getAttribute/PracticeTask2_LinkText.java
@@ -0,0 +1,50 @@
+package com.cydeo.tests.day2_Locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PracticeTask2_LinkText {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://practice.cydeo.com");
+
+        //3- Click to A/B Testing from top of the list.
+        driver.findElement(By.linkText("A/B Testing")).click();
+
+        String expectedTitle = "No A/B Test";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle)) {
+            System.out.println("Title verification PASSED");
+        } else {
+            System.out.println("Title verification FAILED!!!");
+        }
+
+        driver.navigate().back();
+        String expectedTitle2 = "Practice";
+        String actualTitle2 = driver.getTitle();
+
+        if (actualTitle2.equals(expectedTitle2)) {
+            System.out.println("Title verification PASSED");
+        } else {
+            System.out.println("Title verification FAILED!!!");
+        }
+        driver.close();
+    }
+}
+
+/*
+TC #2: Back and forth navigation
+1- Open a chrome browser
+2- Go to: https://practice.cydeo.com
+3- Click to A/B Testing from top of the list.
+4- Verify title is:
+  Expected: No A/B Test
+5- Go back to home page by using the .back();
+6- Verify title equals:
+  Expected: Practice
+ */
