a6a9caeb1391a8b1f7a3e1af9917f7f0bbb73e29
Olesia Sokol <lesy0101@me.com>
2022-02-14 21:02:51 +0700

    day2_Locators_getText_getAttribute

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T5_checkboxes.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T5_checkboxes.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import java.util.concurrent.TimeUnit;
 
 public class T5_checkboxes {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 //        Practice: Checkboxes
 //        1. Go to http://practice.cydeo.com/checkboxes
         WebDriver driver = WebDriverFactory.getDriver("chrome");
@@ -28,11 +28,15 @@ public class T5_checkboxes {
 //        3. Confirm checkbox #2 is SELECTED by default.
         System.out.println("checkbox2.isSelected(), expecting true = " + checkbox2.isSelected());
 //        4. Click checkbox #1 to select it.
-        //checkbox1.click();
+        Thread.sleep(2000);
+        checkbox1.click();
 //        5. Click checkbox #2 to deselect it.
-        //checkbox2.click();
+        Thread.sleep(2000);
+        checkbox2.click();
 //        6. Confirm checkbox #1 is SELECTED.
+        System.out.println("checkbox1.isSelected(), expecting true = " + checkbox1.isSelected());
 //        7. Confirm checkbox #2 is NOT selected.
+        System.out.println("checkbox2.isSelected(), expecting false = " + checkbox2.isSelected());
         driver.close();
     }
 }
