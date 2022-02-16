4d2a2445ee589b00a1dbe5d77d630588bc49ce92
Olesia Sokol <lesy0101@me.com>
2022-02-16 22:06:44 +0700

    day5_testNG_intro_dropdowns

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T3_RadioButton_cont.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T3_RadioButton_cont.java
@@ -40,6 +40,17 @@ public class T3_RadioButton_cont {
                 System.out.println(radioButtonId + " is selected : " + radioButton.isSelected());
                 break;
             }
+            //Create a utility method to handle above logic.
+            //Method name: clickAndVerifyRadioButton
+            //Return type: void or boolean
+            //Method args:
+            //1. WebDriver
+            //2. Name attribute as String (for providing which group of radio buttons)
+            //3. Id attribute as String (for providing which radio button to be clicked)
+            //
+            //Method should loop through the given group of radio buttons. When it finds the
+            //matching option, it should click and verify option is Selected.
+            //Print out verification: true
 
         }
     }
