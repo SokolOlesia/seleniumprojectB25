4cbe6790184ecac60683b050b9a299d70ad83604
Olesia Sokol <lesy0101@me.com>
2022-02-23 22:19:08 +0700

    day6_alerts_iframes_windows

---
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -0,0 +1,65 @@
+package com.cydeo.tests.day7_webtables_utilities_javafaker;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class T1_WindowHandling {
+    WebDriver driver;
+    @BeforeMethod
+    public void setupMethod(){
+        //1. Create a new class called: T5_WindowsPractice
+        //2. Create new test and make set ups
+        //3. Go to :
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.amazon.com ");
+    }
+    @AfterMethod
+    public void teardownMethod() {
+        driver.quit();
+    }
+    @Test
+    public void window_handling_test(){
+        //3. Copy paste the lines from below into your class
+        ((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
+        //
+        //4. Create a logic to switch to the tab where Etsy.com is open
+        Set<String> allWindowHandles = driver.getWindowHandles();
+        for (String allWindowHandle : allWindowHandles) {
+            driver.switchTo().window(allWindowHandle);
+
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+            if(driver.getCurrentUrl().contains("etsy")){
+                break;
+            }
+        }
+
+        //5. Assert: Title contains “Etsy”
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Etsy";
+
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+
+
+
+        //Lines to be pasted:
+        //((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
+        //((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
+        //((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
+        //
+        //These lines will simply open new tabs using something called JavascriptExecutor
+        //and get those pages. We will learn JavascriptExecutor later as well.
+    }
+}
