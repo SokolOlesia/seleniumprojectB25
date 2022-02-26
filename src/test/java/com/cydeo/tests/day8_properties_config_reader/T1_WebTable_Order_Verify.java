e1182cf16cc6359964ea30063f1d2f029bb17178
Olesia Sokol <lesy0101@me.com>
2022-02-26 13:23:04 +0700

    day8_properties_config_reader

---
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
@@ -0,0 +1,56 @@
+package com.cydeo.tests.day8_properties_config_reader;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+//TC #1: Web table practice
+
+public class T1_WebTable_Order_Verify {
+
+    @Test
+    public void order_name_verify_test(){
+        //2. Verify Bob’s name is listed as expected.
+        //Expected: “Bob Martin”
+
+        //Locate the cell that has Bob Martin text in it.
+        WebElement bobMartinCell = driver.findElement(By.xpath("//table[@id = 'ctl00_MainContent_orderGrid']//td[.='Bob Martin']"));
+
+        String expectedBobName = "Bob Martin";
+        String actualBobName = bobMartinCell.getText();
+
+        Assert.assertEquals(expectedBobName,actualBobName);
+
+        //3. Verify Bob Martin’s order date is as expected
+        //Expected: 12/31/2021
+
+        // alternative locator to bobMartinDateCell = "//td[.='Bob Martin']/../td[5]"
+        WebElement bobMartinDateCell = driver.findElement(By.xpath("//table[@id = 'ctl00_MainContent_orderGrid']//td[.='Bob Martin']/following-sibling::td[3]"));
+
+        String expectedBobDate = "12/31/2021";
+        String actualBobDate = bobMartinDateCell.getText();
+
+        Assert.assertEquals(expectedBobDate,actualBobDate);
+    }
+
+
+    public WebDriver driver;
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/web-tables");
+    }
+    @AfterMethod
+    public void teardownMethod() {
+        driver.quit();
+    }
+}
