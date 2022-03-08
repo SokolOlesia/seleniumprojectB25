d96a49126887c3cc70a15dba646054c9082a9735
Olesia Sokol <lesy0101@me.com>
2022-03-08 11:27:10 -0500

    day10_upload_actions_jsexecuter

--- src/test/java/com/cydeo/tests/base/TestBase.java
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -5,13 +5,15 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import static com.cydeo.utilities.ConfigurationReader.getProperty;
+
 public class TestBase {
     protected WebDriver driver;
 
     @BeforeMethod
     public void setUp(){
-        //Driver.getDriver().get(getProperty("env"));
-        Driver.getDriver();
+        Driver.getDriver().get(getProperty("registration_form_URl"));
+        //Driver.getDriver();
     }
     @AfterMethod
     public void tearDown(){
