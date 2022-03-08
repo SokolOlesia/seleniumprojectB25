ea1abdfdc61d77f960029b84a9cc95a647e196e6
Olesia Sokol <lesy0101@me.com>
2022-03-08 10:48:22 -0500

    day9_javafaker

--- src/test/java/com/cydeo/tests/base/TestBase.java
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -1,26 +1,22 @@
 package com.cydeo.tests.base;
 
-import com.cydeo.utilities.WebDriverFactory;
+import com.cydeo.utilities.Driver;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.util.concurrent.TimeUnit;
+import static com.cydeo.utilities.ConfigurationReader.getProperty;
 
 public class TestBase {
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setupMethod(){
-        driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
+    public void setUp(){
+        Driver.getDriver().get(getProperty("env"));
     }
-
     @AfterMethod
     public void tearDown(){
-        driver.close();
+        Driver.closeDriver();
     }
 
 }
--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -26,9 +26,9 @@ public class Driver {
     /*
     Create a re-usable utility method which will return same driver instance when we call it
      */
-    public static WebDriver getDriver() {
+    public static WebDriver getDriver(){
 
-        if (driver == null) {
+        if (driver == null){
 
             /*
             We read our browserType from configuration.properties.
@@ -41,8 +41,7 @@ public class Driver {
                 Depending on the browserType that will be return from configuration.properties file
                 switch statement will determine the case, and open the matching browser
             */
-
-            switch (browserType) {
+            switch (browserType){
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
@@ -54,8 +53,18 @@ public class Driver {
                     driver = new FirefoxDriver();
                     driver.manage().window().maximize();
                     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+
             }
         }
+
         return driver;
+
+    }
+    public static void closeDriver(){
+        if (driver != null){
+            driver.quit();
+            driver=null;
+        }
     }
 }
