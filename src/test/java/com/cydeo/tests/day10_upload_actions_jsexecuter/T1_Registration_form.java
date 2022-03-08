bc79b28dded49d5d0d3c9da352ae78261891bb40
Olesia Sokol <lesy0101@me.com>
2022-03-08 11:55:38 -0500

    day10_upload_actions_jsexecuter

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecuter/T1_Registration_form.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecuter/T1_Registration_form.java
@@ -2,26 +2,35 @@ package com.cydeo.tests.day10_upload_actions_jsexecuter;
 
 import com.cydeo.tests.base.TestBase;
 import com.cydeo.utilities.Driver;
+import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class T1_Registration_form extends TestBase {
     @Test
-    public void registration_test(){
+    public void registration_test() {
 //        Note: Use JavaFaker OR read from configuration.properties file when possible.
 //        1. Open browser
 //        2. Go to website: https://practice.cydeo.com/registration_form
 
-
+        Faker faker = new Faker();
 //        3. Enter first name
 
- WebElement firstName = Driver.getDriver().findElement(By.xpath("//input[@name='firstname']"));
-//        firstName.sendKeys(faker.name().firstName());
+        WebElement firstName = Driver.getDriver().findElement(By.xpath("//input[@name='firstname']"));
+        firstName.sendKeys(faker.name().firstName());
 //        4. Enter last name
+        WebElement lastName = Driver.getDriver().findElement(By.xpath("//input[@name='lastname']"));
+        lastName.sendKeys(faker.name().lastName());
 //        5. Enter username
+        WebElement userName = Driver.getDriver().findElement(By.xpath("//input[@name='uername']"));
+        userName.sendKeys(faker.name().username());
 //        6. Enter email address
+        WebElement email = Driver.getDriver().findElement(By.xpath("//input[@name='email']"));
+        email.sendKeys(faker.internet().emailAddress());
 //        7. Enter password
+        WebElement password = Driver.getDriver().findElement(By.xpath("//input[@name='password']"));
+        password.sendKeys(faker.internet().password());
 //        8. Enter phone number
 //        9. Select a gender from radio buttons
 //        10. Enter date of birth
