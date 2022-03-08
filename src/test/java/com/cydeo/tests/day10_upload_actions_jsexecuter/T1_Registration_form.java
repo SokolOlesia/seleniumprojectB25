8e2fa3eb6bde72296f9fb780a4d78623e38b701a
Olesia Sokol <lesy0101@me.com>
2022-03-08 13:01:32 -0500

    day10_upload_actions_jsexecuter

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecuter/T1_Registration_form.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecuter/T1_Registration_form.java
@@ -5,11 +5,16 @@ import com.cydeo.utilities.Driver;
 import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 public class T1_Registration_form extends TestBase {
     @Test
-    public void registration_test() {
+    public void registration_test() throws InterruptedException {
 //        Note: Use JavaFaker OR read from configuration.properties file when possible.
 //        1. Open browser
 //        2. Go to website: https://practice.cydeo.com/registration_form
@@ -19,27 +24,61 @@ public class T1_Registration_form extends TestBase {
 
         WebElement firstName = Driver.getDriver().findElement(By.xpath("//input[@name='firstname']"));
         firstName.sendKeys(faker.name().firstName());
+
 //        4. Enter last name
         WebElement lastName = Driver.getDriver().findElement(By.xpath("//input[@name='lastname']"));
         lastName.sendKeys(faker.name().lastName());
+
 //        5. Enter username
-        WebElement userName = Driver.getDriver().findElement(By.xpath("//input[@name='uername']"));
-        userName.sendKeys(faker.name().username());
+        WebElement userName = Driver.getDriver().findElement(By.xpath("//input[@name='username']"));
+        userName.sendKeys(faker.bothify("?????????#?"));
+
 //        6. Enter email address
         WebElement email = Driver.getDriver().findElement(By.xpath("//input[@name='email']"));
         email.sendKeys(faker.internet().emailAddress());
+       // email.sendKeys(userName + "@email.com");
+
 //        7. Enter password
         WebElement password = Driver.getDriver().findElement(By.xpath("//input[@name='password']"));
-        password.sendKeys(faker.internet().password());
+        //password.sendKeys(faker.internet().password());
+        password.sendKeys(faker.numerify("#########"));
+
 //        8. Enter phone number
+        WebElement phoneNumber = Driver.getDriver().findElement(By.xpath("//input[@name='phone']"));
+        phoneNumber.sendKeys(faker.numerify("571-###-####"));
+
 //        9. Select a gender from radio buttons
+        WebElement genderButton = Driver.getDriver().findElement(By.xpath("//input[@value='male']"));
+        genderButton.click();
+
 //        10. Enter date of birth
+        WebElement dateOfBirth = Driver.getDriver().findElement(By.xpath("//input[@name='birthday']"));
+        dateOfBirth.sendKeys(faker.date().birthday().toInstant().atOffset(ZoneOffset.UTC).format(DateTimeFormatter.ofPattern("MM/dd/yyyy")));
+
 //        11. Select Department/Office
+        Select department = new Select(Driver.getDriver().findElement(By.xpath("//select[@name='department']")));
+        department.selectByVisibleText("Department of Engineering");
+
 //        12. Select Job Title
+        Select jobTitle = new Select(Driver.getDriver().findElement(By.xpath("//select[@name='job_title']")));
+        jobTitle.selectByIndex(5);
+
 //        13. Select programming language from checkboxes
+        WebElement language = Driver.getDriver().findElement(By.xpath("//input[@id='inlineCheckbox2']"));
+        language.click();
+
+        //Thread.sleep(5000);
 //        14. Click to sign up button
+        WebElement singUpBt = Driver.getDriver().findElement(By.xpath("//button[@id='wooden_spoon']"));
+        singUpBt.click();
+
 //        15. Verify success message “You’ve successfully completed registration.” is
 //        displayed.
+        WebElement message = Driver.getDriver().findElement(By.xpath("//p"));
+
+        String actualMessage = message.getText();
+        String expectedMessage = "You've successfully completed registration!";
+        Assert.assertEquals(actualMessage, expectedMessage);
 //
 //
 //                Note:
